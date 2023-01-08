e446e53ef82f7e723d7f68a8d7d732bf44af4f94
Alex MacArthur <alex@macarthur.me>
2023-01-08 13:17:10 -0600

    need to wire up feedback endpoint.

---
+++ src/scripts/feedback.ts
@@ -0,0 +1,18 @@
+// const slug = url.split("/posts/").reverse()[0];
+
+// const handleFeedback = (e) => {
+//   console.log(e);
+//   // const {
+//   //   detail: { value },
+//   // } = e;
+
+//   // fetch(`/api/feedback`, {
+//   //   method: "POST",
+//   //   headers: {
+//   //     "Content-Type": "application/json",
+//   //   },
+//   //   body: JSON.stringify({ slug, value }),
+//   // });
+// };
+
+// document.addEventListener("feedback:interaction", handleFeedback);
