6b143f7fe8c0ad4beb89349bb4bdf7c9e0198606
Alex MacArthur <alex@macarthur.me>
2023-03-12 20:36:29 -0500

    Disable compression for now.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import image from "@astrojs/image";
 import sitemap from "@astrojs/sitemap";
-import compress from "astro-compress";
+// import compress from "astro-compress";
 import robotsTxt from "astro-robots-txt";
 
 export default defineConfig({
@@ -17,12 +17,12 @@ export default defineConfig({
         { disallow: "/proxy/", userAgent: "*" },
       ],
     }),
-    compress({
-      css: false,
-      html: true,
-      img: true,
-      js: false,
-      svg: true,
-    }),
+    // compress({
+    //   css: false,
+    //   html: true,
+    //   img: true,
+    //   js: false,
+    //   svg: true,
+    // }),
   ],
 });
