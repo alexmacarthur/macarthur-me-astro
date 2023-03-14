9bfe94a9d0dc57977108a8e0540f381d6998ca74
Alex MacArthur <alex@macarthur.me>
2023-03-13 23:48:50 -0500

    Run on the edge.

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,25 +4,26 @@ import image from "@astrojs/image";
 import sitemap from "@astrojs/sitemap";
 import compress from "astro-compress";
 import robotsTxt from "astro-robots-txt";
+import vercel from '@astrojs/vercel/edge';
 
+// https://astro.build/config
 export default defineConfig({
   site: "https://macarthur.me",
-  integrations: [
-    tailwind(),
-    image(),
-    sitemap(),
-    robotsTxt({
-      policy: [
-        { allow: "/", userAgent: "*" },
-        { disallow: "/proxy/", userAgent: "*" },
-      ],
-    }),
-    compress({
-      css: false,
-      html: true,
-      img: true,
-      js: false,
-      svg: true,
-    }),
-  ],
+  integrations: [tailwind(), image(), sitemap(), robotsTxt({
+    policy: [{
+      allow: "/",
+      userAgent: "*"
+    }, {
+      disallow: "/proxy-image/",
+      userAgent: "*"
+    }]
+  }), compress({
+    css: false,
+    html: true,
+    img: true,
+    js: false,
+    svg: true
+  })],
+  output: "server",
+  adapter: vercel()
 });
