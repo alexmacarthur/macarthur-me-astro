84eaaa1469dbb317d04260c1895317a681d67b94
Alex MacArthur <alex@macarthur.me>
2023-05-23 22:15:43 -0500

    Set fallback fonts.

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,7 +4,7 @@ module.exports = {
   theme: {
     fontFamily: {
       sans: [
-        '"Inter Variable", monospace',
+        '"Inter Variable", ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif',
       ],
     },
     fontWeight: {
