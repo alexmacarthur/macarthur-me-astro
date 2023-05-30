24151139873c3accc8fa202f6ffc65056fc268f2
Alex MacArthur <alex@macarthur.me>
2023-05-30 18:49:00 -0500

    Simplify fallback font.

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,7 +4,7 @@ module.exports = {
   theme: {
     fontFamily: {
       sans: [
-        '"Inter Variable", ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif',
+        '"Inter Variable", sans-serif',
       ],
     },
     fontWeight: {
