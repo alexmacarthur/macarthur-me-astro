b1aed575bef34f4cdd47cd4fac80201f3a48b1fe
Alex MacArthur <alex@macarthur.me>
2024-03-07 19:17:41 -0600

    change cache headers

---
+++ functions/_middleware.ts
@@ -0,0 +1,11 @@
+// Set CORS to all /api responses
+export const onRequest: PagesFunction = async (context) => {
+  const response = await context.next();
+
+  response.headers.set(
+    "Cache-Control",
+    "public, s-maxage=3600, stale-while-revalidate=43200"
+  );
+
+  return response;
+};
