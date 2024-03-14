0de30f91e4332c1fd5aa0446092f88fcbf9a3b39
Alex MacArthur <alex@macarthur.me>
2024-03-13 20:42:07 -0500

    append version

--- functions/open-graph.ts
+++ functions/open-graph.ts
@@ -1,8 +1,10 @@
 export const onRequest: PagesFunction = async (context) => {
-  const urlParams = new URL(context.request.url).searchParams.toString();
+  const urlParams = new URL(context.request.url).searchParams;
+
+  urlParams.append("v", "2");
 
   return fetch(
-    `https://macarthur-me-api.vercel.app/api/og?${urlParams}`,
+    `https://macarthur-me-api.vercel.app/api/og?${urlParams.toString()}`,
     context.request
   );
 };
