b0f57176773845d1338a6f77d4fad81413958b5b
Alex MacArthur <alex@macarthur.me>
2025-01-17 22:05:00 -0600

    further event handling clean-up

--- src/scripts/search-trigger.ts
+++ src/scripts/search-trigger.ts
@@ -1,13 +1,27 @@
 import { sendEvent } from "../utils";
 
+let isBeingHandled = false;
+
+export function handleTrigger() {
+  if (isBeingHandled) {
+    return;
+  }
+
+  isBeingHandled = true;
+
+  document.body.dispatchEvent(new CustomEvent("search:open"));
+
+  sendEvent("search_open", {
+    method: "input-trigger",
+  });
+
+  isBeingHandled = false;
+}
+
 document.addEventListener("astro:page-load", function () {
   const searchTrigger = document.getElementById("searchTrigger") as HTMLElement;
 
-  searchTrigger.addEventListener("click", () => {
-    document.body.dispatchEvent(new CustomEvent("search:open"));
-
-    sendEvent("search_open", {
-      method: "input-trigger",
-    });
+  document.querySelectorAll(".search-trigger").forEach((trigger) => {
+    trigger.addEventListener("click", handleTrigger);
   });
 });
