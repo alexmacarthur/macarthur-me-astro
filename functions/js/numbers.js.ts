0dd4abe01b69d7fa5c20f4fd4025d456e31b0297
Alex MacArthur <alex@macarthur.me>
2025-02-03 19:15:33 -0600

    Update numbers endpoint.

--- functions/js/numbers.js.ts
+++ functions/js/numbers.js.ts
@@ -1,6 +1,3 @@
 export const onRequest: PagesFunction = async (context) => {
-  return fetch(
-    "https://analytics.macarthur.me/js/plausible.js",
-    context.request,
-  );
+  return fetch("https://analytics.macarthur.me/js/script.js", context.request);
 };
