490d973a9e14cdfc2f2edfd1747583f667ff05f6
Alex MacArthur <alex@macarthur.me>
2025-02-22 17:51:30 -0700

    Fix prose settings.

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -22,26 +22,26 @@ module.exports = {
             maxWidth: "100%",
             img: { margin: "0 auto", maxWidth: "100%" },
             figcaption: { textAlign: "center" },
-            a: { color: "var('--color-gray-800')", textDecoration: "none" },
+            a: { color: "var(--color-gray-800)", textDecoration: "none" },
             "a code": {
               background: "none",
               fontSize: "inherit",
               fontFamily: "inherit",
               padding: "0",
             },
-            strong: { color: "var('--color-gray-800')" },
+            strong: { color: "var(--color-gray-800)" },
             "h1, h2, h3, h4, h5, h6": {
-              color: "var('--color-gray-900')",
+              color: "var(--color-gray-900)",
               marginBottom: `theme("margin.3") !important`,
               fontWeight: `650`,
             },
             "h1 a, h2 a, h3 a, h4 a, h5 a, h6 a": { fontWeight: "inherit" },
             code: {
               fontWeight: "inherit",
-              color: "var('--color-gray-700')",
+              color: "var(--color-gray-700)",
               borderRadius: theme("border.sm"),
               padding: theme("padding.1"),
-              background: "var('--color-gray-200')",
+              background: "var(--color-gray-200)",
             },
             "code::before": { content: "none" },
             "code::after": { content: "none" },
@@ -55,8 +55,8 @@ module.exports = {
 
   plugins: [
     require("@tailwindcss/typography"),
-    function ({ addVariant, addUtilities, theme }) {
-      addUtilities({ ".red-copy *": { color: "var('--color-rose-600')" } });
+    function ({ addVariant, addUtilities }) {
+      addUtilities({ ".red-copy *": { color: "var(--color-rose-600)" } });
       addVariant("child-link", "& > a");
       addVariant("child-link-hover", "& > a:hover");
     },
