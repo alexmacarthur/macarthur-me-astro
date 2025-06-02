eb5db5042a228f35aa9d1b9e029fdf8f17e3feb5
Alex MacArthur <alex@macarthur.me>
2025-06-02 07:48:09 -0500

    Update link-tracking.ts

--- src/scripts/link-tracking.ts
+++ src/scripts/link-tracking.ts
@@ -2,8 +2,6 @@ import { sendEvent } from "../utils";
 
 const links = document.querySelectorAll("a[data-link-id]");
 
-console.log(links);
-
 links.forEach((link) => {
   const linkId = (link as HTMLElement).dataset.linkId as string;
 
